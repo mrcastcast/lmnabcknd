ffd7158a3adc6a5284ce8ef26e1563af7d424253
mrcastcast <158112672+mrcastcast@users.noreply.github.com>
2026-05-08 16:58:35 +0200

    Update Payment.js

--- models/Payment.js
+++ models/Payment.js
@@ -1,20 +1,69 @@
 const mongoose = require("mongoose");
 
 const paymentSchema = new mongoose.Schema({
-  paymentId: { type: String, required: true, unique: true },
-  fullName: { type: String, default: "Demo User" },
-  email: { type: String, default: "demo@lumina.local" },
-  planNumber: { type: Number, required: true },
-  planName: { type: String, required: true },
-  amount: { type: Number, required: true },
-  network: { type: String, default: "USDT TRC20" },
-  txid: { type: String, required: true },
+
+  paymentId: {
+    type: String,
+    required: true,
+    unique: true
+  },
+
+  fullName: {
+    type: String,
+    default: "Demo User"
+  },
+
+  email: {
+    type: String,
+    default: "demo@lumina.local"
+  },
+
+  planNumber: {
+    type: Number,
+    required: true
+  },
+
+  planName: {
+    type: String,
+    required: true
+  },
+
+  amount: {
+    type: Number,
+    required: true
+  },
+
+  network: {
+    type: String,
+    default: "USDT TRC20"
+  },
+
+  txid: {
+    type: String,
+    default: ""
+  },
+
+  proofImage: {
+    type: String,
+    default: ""
+  },
+
+  proofImageName: {
+    type: String,
+    default: ""
+  },
+
   status: {
     type: String,
     enum: ["pending", "approved", "rejected"],
     default: "pending"
   },
-  createdAt: { type: Date, default: Date.now }
+
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+
 });
 
 module.exports = mongoose.model("Payment", paymentSchema);
